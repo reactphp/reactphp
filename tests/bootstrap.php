53071ffd578429e6ba627d7450b15320872c074e
Igor Wiedler <igor@wiedler.ch>
2012-04-21 18:09:34 +0200

    Use new composer autoload filename

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 
-$loader = require __DIR__.'/../vendor/.composer/autoload.php';
+$loader = require __DIR__.'/../vendor/autoload.php';
 $loader->add('Igorw\Tests', __DIR__);
 $loader->register();
