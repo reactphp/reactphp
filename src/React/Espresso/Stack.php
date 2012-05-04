bd50cef9bf5ef5921841f956edcd667b52c95b81
Igor Wiedler <igor@wiedler.ch>
2012-05-04 12:15:47 +0200

    Do not make Stack extend Application

--- src/React/Espresso/Stack.php
+++ src/React/Espresso/Stack.php
@@ -6,28 +6,28 @@ use React\EventLoop\Factory;
 use React\Socket\Server as SocketServer;
 use React\Http\Server as HttpServer;
 
-class Stack extends Application
+class Stack extends \Pimple
 {
-    public function __construct()
+    public function __construct(Application $app)
     {
-        parent::__construct();
-
         $this['loop'] = $this->share(function () {
             return Factory::create();
         });
 
-        $this['socket'] = $this->share(function ($app) {
-            return new SocketServer($app['loop']);
+        $this['socket'] = $this->share(function ($stack) {
+            return new SocketServer($stack['loop']);
         });
 
-        $this['http'] = $this->share(function ($app) {
-            return new HttpServer($app['socket']);
+        $this['http'] = $this->share(function ($stack) {
+            return new HttpServer($stack['socket']);
         });
+
+        $this['app'] = $app;
     }
 
     public function listen($port, $host = '127.0.0.1')
     {
-        $this['http']->on('request', $this);
+        $this['http']->on('request', $this['app']);
         $this['socket']->listen($port, $host);
         $this['loop']->run();
     }
