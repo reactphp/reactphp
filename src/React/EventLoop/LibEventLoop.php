5c00bdf9e14d4fedbcecabd796a30b4985b9a221
Daniele Alessandri <suppakilla@gmail.com>
2012-05-05 10:40:40 +0200

    Reimplement the libevent-based event loop.
    The previous implementation was based on wrong assumptions on how the
    PECL libevent extension works. Please note that this class must be used
    with PECL libevent v0.0.5 since they fixed a bug in which "event_set()"
    was resetting the event base.
    
    Due to a bug, most likely in the PECL extension, this class is not able
    to use file streams as it considers them invalid streams. There is an
    open issue on the PECL bug tracker for the libevent project:
    
      https://bugs.php.net/bug.php?id=59728
    
    Everything works fine with socket streams and STDIN, STDOUT, STDERR, etc.

--- src/React/EventLoop/LibEventLoop.php
+++ src/React/EventLoop/LibEventLoop.php
@@ -5,41 +5,137 @@ namespace React\EventLoop;
 class LibEventLoop implements LoopInterface
 {
     private $base;
+    private $callback;
+
     private $events = array();
+    private $flags = array();
+    private $readCallbacks = array();
+    private $writeCallbacks = array();
 
     public function __construct()
     {
         $this->base = event_base_new();
+        $this->callback = $this->createLibeventCallback();
+    }
+
+    protected function createLibeventCallback()
+    {
+        $readCbks = &$this->readCallbacks;
+        $writeCbks = &$this->writeCallbacks;
+
+        return function($stream, $flags, $loop) use(&$readCbks, &$writeCbks) {
+            $streamID = (int) $stream;
+
+            try {
+                if (($flags & EV_READ) === EV_READ && isset($readCbks[$streamID])) {
+                    if (call_user_func($readCbks[$streamID], $stream, $loop) === false) {
+                        $loop->removeReadStream($stream);
+                    }
+                }
+
+                if (($flags & EV_WRITE) === EV_WRITE && isset($writeCbks[$streamID])) {
+                    if (call_user_func($writeCbks[$streamID], $stream, $loop) === false) {
+                        $loop->removeWriteStream($stream);
+                    }
+                }
+            } catch (\Exception $ex) {
+                // If one of the callbacks throws an exception we must remove the stream
+                // otherwise libevent will swallow the exception and go berserk.
+                $loop->removeStream($stream);
+
+                throw $ex;
+            }
+        };
     }
 
     public function addReadStream($stream, $listener)
     {
-        $event = event_new();
-        event_set($event, $stream, EV_READ | EV_PERSIST, $listener);
-        event_base_set($event, $this->base);
-        event_add($event);
-
-        $this->events[(int) $stream] = $event;
+        $this->addStreamEvent($stream, EV_READ, 'read', $listener);
     }
 
     public function addWriteStream($stream, $listener)
     {
-        $event = event_new();
-        event_set($event, $stream, EV_READ | EV_PERSIST, $listener);
-        event_base_set($event, $this->base);
+        $this->addStreamEvent($stream, EV_WRITE, 'write', $listener);
+    }
+
+    protected function addStreamEvent($stream, $eventClass, $eventCallbacks, $listener)
+    {
+        $streamID = (int) $stream;
+
+        if ($existing = isset($this->events[$streamID])) {
+            if (($this->flags[$streamID] & $eventClass) === $eventClass) {
+                return;
+            }
+            $event = $this->events[$streamID];
+            event_del($event);
+        } else {
+            $event = event_new();
+        }
+
+        $flags = isset($this->flags[$streamID]) ? $this->flags[$streamID] | $eventClass : $eventClass;
+        event_set($event, $stream, $flags | EV_PERSIST, $this->callback, $this);
+
+        if (!$existing) {
+            // Set the base only if $event has been newly created or be ready for segfaults.
+            event_base_set($event, $this->base);
+        }
+
         event_add($event);
 
-        $this->events[(int) $stream] = $event;
+        $this->events[$streamID] = $event;
+        $this->flags[$streamID] = $flags;
+        $this->{"{$eventCallbacks}Callbacks"}[$streamID] = $listener;
+    }
+
+    public function removeReadStream($stream)
+    {
+        $this->removeStreamEvent($stream, EV_READ, 'read');
+    }
+
+    public function removeWriteStream($stream)
+    {
+        $this->removeStreamEvent($stream, EV_WRITE, 'write');
+    }
+
+    protected function removeStreamEvent($stream, $eventClass, $eventCallbacks)
+    {
+        $streamID = (int) $stream;
+
+        if (isset($this->events[$streamID])) {
+            $flags = $this->flags[$streamID] & ~$eventClass;
+
+            if ($flags === 0) {
+                // Remove if stream is not subscribed to any event at this point.
+                return $this->removeStream($stream);
+            }
+
+            $event = $this->events[$streamID];
+
+            event_del($event);
+            event_set($event, $stream, $flags | EV_PERSIST, $this->callback, $this);
+            event_add($event);
+
+            $this->flags[$streamID] = $flags;
+            unset($this->{"{$eventCallbacks}Callbacks"}[$streamID]);
+        }
     }
 
     public function removeStream($stream)
     {
-        if (isset($this->events[(int) $stream])) {
-            $event = $this->events[(int) $stream];
+        $streamID = (int) $stream;
+
+        if (isset($this->events[$streamID])) {
+            $event = $this->events[$streamID];
+
             event_del($event);
             event_free($event);
 
-            unset($this->events[(int) $stream]);
+            unset(
+                $this->events[$streamID],
+                $this->flags[$streamID],
+                $this->readCallbacks[$streamID],
+                $this->writeCallbacks[$streamID]
+            );
         }
     }
 
