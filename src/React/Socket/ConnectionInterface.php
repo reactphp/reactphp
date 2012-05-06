46bb206d21128d5bbc2c667eb3c86c4769bd5906
Chris Boden <cboden@gmail.com>
2012-05-06 12:00:56 -0400

    [Socket] Removed __cons from interface

--- src/React/Socket/ConnectionInterface.php
+++ src/React/Socket/ConnectionInterface.php
@@ -7,7 +7,6 @@ use React\EventLoop\LoopInterface;
 
 interface ConnectionInterface extends EventEmitterInterface
 {
-    public function __construct($socket, LoopInterface $loop);
     public function write($data);
     public function end();
     public function handleData($socket);
