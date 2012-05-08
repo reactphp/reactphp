04fc88f801c85ce9e22acc8cdee75ca43082658b
Igor Wiedler <igor@wiedler.ch>
2012-05-09 00:20:20 +0200

    [EventLoop] Addd remove[Read|Write]Stream to LoopInterface

--- src/React/EventLoop/LoopInterface.php
+++ src/React/EventLoop/LoopInterface.php
@@ -7,6 +7,8 @@ interface LoopInterface
     public function addReadStream($stream, $listener);
     public function addWriteStream($stream, $listener);
 
+    public function removeReadStream($stream);
+    public function removeWriteStream($stream);
     public function removeStream($stream);
 
     public function tick();
