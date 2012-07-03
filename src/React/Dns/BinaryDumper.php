90fe583443a193c7ec9272dfbd3a0bfc5c605261
Igor Wiedler <igor@wiedler.ch>
2012-07-04 01:46:53 +0200

    [Dns] Add basic BinaryDumper implementation and test

--- src/React/Dns/BinaryDumper.php
+++ src/React/Dns/BinaryDumper.php
@@ -6,5 +6,54 @@ class BinaryDumper
 {
     public function toBinary(Message $message)
     {
+        $data = '';
+
+        $data .= $this->headerToBinary($message->header);
+        $data .= $this->questionToBinary($message->question);
+
+        return $data;
+    }
+
+    private function headerToBinary(array $header)
+    {
+        $data = '';
+
+        $data .= pack('n', $header['id']);
+
+        $flags = 0x00;
+        $flags = ($flags << 1) | $header['qr'];
+        $flags = ($flags << 4) | $header['opcode'];
+        $flags = ($flags << 1) | $header['aa'];
+        $flags = ($flags << 1) | $header['tc'];
+        $flags = ($flags << 1) | $header['rd'];
+        $flags = ($flags << 1) | $header['ra'];
+        $flags = ($flags << 3) | $header['z'];
+        $flags = ($flags << 4) | $header['rcode'];
+
+        $data .= pack('n', $flags);
+
+        $data .= pack('n', $header['qdCount']);
+        $data .= pack('n', $header['anCount']);
+        $data .= pack('n', $header['nsCount']);
+        $data .= pack('n', $header['arCount']);
+
+        return $data;
+    }
+
+    private function questionToBinary(array $question)
+    {
+        $data = '';
+
+        foreach ($question as $q) {
+            $labels = explode('.', $q['name']);
+            foreach ($labels as $label) {
+                $data .= chr(strlen($label)).$label;
+            }
+            $data .= "\x00";
+
+            $data .= pack('n*', $q['type'], $q['class']);
+        }
+
+        return $data;
     }
 }
---
+++ tests/React/Tests/Dns/BinaryDumperTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace React\Tests\Dns;
+
+use React\Dns\BinaryDumper;
+use React\Dns\Message;
+
+class BinaryDumperTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRequestToBinary()
+    {
+        $data = "";
+        $data .= "72 62 01 00 00 01 00 00 00 00 00 00"; // header
+        $data .= "04 69 67 6f 72 02 69 6f 00";          // question: igor.io
+        $data .= "00 01 00 01";                         // question: type A, class IN
+
+        $expected = $this->formatHexDump(str_replace(' ', '', $data), 2);
+
+        $request = new Message();
+        $request->header = array(
+            'id'        => 0x7262,
+            'qdCount'   => 1,
+            'anCount'   => 0,
+            'nsCount'   => 0,
+            'arCount'   => 0,
+            'qr'        => 0,
+            'opcode'    => Message::OPCODE_QUERY,
+            'aa'        => 0,
+            'tc'        => 0,
+            'rd'        => 1,
+            'ra'        => 0,
+            'z'         => 0,
+            'rcode'     => Message::RCODE_OK,
+        );
+        $request->question[] = array(
+            'name'  => 'igor.io',
+            'type'  => Message::TYPE_A,
+            'class' => Message::CLASS_IN,
+        );
+
+        $dumper = new BinaryDumper();
+        $data = $dumper->toBinary($request);
+        $data = $this->convertBinaryToHexDump($data);
+
+        $this->assertSame($expected, $data);
+    }
+
+    private function convertBinaryToHexDump($input)
+    {
+        return $this->formatHexDump(implode('', unpack('H*', $input)));
+    }
+
+    private function formatHexDump($input)
+    {
+        return implode(' ', str_split($input, 2));
+    }
+}
