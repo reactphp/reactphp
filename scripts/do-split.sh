9f0a98cd9fab8744670b863c2800119703b67674
Igor Wiedler <igor@wiedler.ch>
2012-07-14 02:42:33 +0200

    Replacing crappy scripts with the amazing git-subsplit

---
+++ scripts/do-split.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+GIT_SUBSPLIT=$(pwd)/$(dirname $0)/git-subsplit.sh
+
+$GIT_SUBSPLIT init https://github.com/react-php/react
+
+$GIT_SUBSPLIT update
+
+$GIT_SUBSPLIT publish "
+    src/React/EventLoop:git@github.com:react-php/event-loop.git
+    src/React/Stream/:git@github.com:react-php/stream.git
+    src/React/Socket/:git@github.com:react-php/socket.git
+    src/React/Http/:git@github.com:react-php/http.git
+    src/React/Espresso/:git@github.com:react-php/espresso.git
+" --heads=master
