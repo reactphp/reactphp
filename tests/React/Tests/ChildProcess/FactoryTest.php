a1ad4ee6418f4191d3b250f815c0f8648714f9e1
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-08-19 05:16:49 +0900

    Add more tests for React\ChildProcess\Factory->spawn()

--- tests/React/Tests/ChildProcess/FactoryTest.php
+++ tests/React/Tests/ChildProcess/FactoryTest.php
@@ -25,6 +25,40 @@ class FactoryTest extends \PHPUnit_Framework_TestCase
         $this->assertSame("cwd = {$cwd}, count of env = 0", $capturedData);
     }
 
+    public function testSpawnWithPwd()
+    {
+        $loop    = $this->createLoop();
+        $factory = $this->createFactory($loop);
+        $process = $factory->spawn('php', array('-r', 'echo "cwd = ", getcwd();'), '/');
+
+        $capturedData = '';
+
+        $process->stdout->on('data', function ($data) use (&$capturedData) {
+            $capturedData .= $data;
+        });
+
+        $loop->run();
+
+        $this->assertSame('cwd = /', $capturedData);
+    }
+
+    public function testSpawnWithEnv()
+    {
+        $loop    = $this->createLoop();
+        $factory = $this->createFactory($loop);
+        $process = $factory->spawn('php', array('-r', 'echo "foo = ", $_ENV["foo"];'), null, array('foo' => 'FOO'));
+
+        $capturedData = '';
+
+        $process->stdout->on('data', function ($data) use (&$capturedData) {
+            $capturedData .= $data;
+        });
+
+        $loop->run();
+
+        $this->assertSame('foo = FOO', $capturedData);
+    }
+
     private function createFactory($loop)
     {
         return new Factory($loop);
