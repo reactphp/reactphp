1643f787b435de2f916f653407f712e2a0224a2b
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-08-18 20:37:05 +0900

    Add some methods

--- examples/child-process.php
+++ examples/child-process.php
@@ -8,6 +8,8 @@ $loop = new React\EventLoop\StreamSelectLoop();
 $factory = new React\ChildProcess\factory($loop);
 
 $processA = $factory->spawn('php', array('-r', 'foreach (range(1, 3) as $i) { echo $i, PHP_EOL; sleep(1); } fputs(STDERR, "Bye.");'));
+echo '[A] ' . blue('[PID]') . '    pid is ', (string) $processA->getPid(), PHP_EOL;
+echo '[A] ' . blue('[CMD]') . '    ', $processA->getCommand(), PHP_EOL;
 
 $processA->stdout->on('data', function ($data) {
     echo '[A] ' . green('[STDOUT]') . ' ';
@@ -24,6 +26,8 @@ $processA->on('exit', function ($status) {
 });
 
 $processB = $factory->spawn('php', array('-r', 'foreach (range(1, 6) as $i) { echo $i, PHP_EOL; sleep(1); } fputs(STDERR, "Bye.");'));
+echo '[B] ' . blue('[PID]') . '    pid is ', (string) $processB->getPid(), PHP_EOL;
+echo '[B] ' . blue('[CMD]') . '    ', $processB->getCommand(), PHP_EOL;
 
 $processB->stdout->on('data', function ($data) {
     echo '[B] ' . green('[STDOUT]') . ' ';
@@ -51,4 +55,8 @@ function green($str) {
 
 function yellow($str) {
     return "\033[33m{$str}\033[0m";
+}
+
+function blue($str) {
+    return "\033[34m{$str}\033[0m";
 }
