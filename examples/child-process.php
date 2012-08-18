e5ec4166bfab7481d2134a46241a64d6eea1393f
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-08-18 20:00:28 +0900

    Add color for example

--- examples/child-process.php
+++ examples/child-process.php
@@ -7,38 +7,48 @@ require __DIR__.'/../vendor/autoload.php';
 $loop = new React\EventLoop\StreamSelectLoop();
 $factory = new React\ChildProcess\factory($loop);
 
-$processA = $factory->spawn('php', array('-r', 'foreach (range(1, 3) as $i) { echo $i, PHP_EOL; sleep(1); } '));
+$processA = $factory->spawn('php', array('-r', 'foreach (range(1, 3) as $i) { echo $i, PHP_EOL; sleep(1); } fputs(STDERR, "Bye.");'));
 
 $processA->stdout->on('data', function ($data) {
-    echo "[A] [STDOUT]: ";
+    echo '[A] ' . green('[STDOUT]') . ' ';
     var_dump($data);
 });
 
 $processA->stderr->on('data', function ($data) {
-    echo "[A] [STDERR]: ";
+    echo '[A] ' . red('[STDERR]') . ' ';
     var_dump($data);
 });
 
 $processA->on('exit', function ($status) {
-    echo "[A] [EXIT]: ";
-    var_dump($status);
+    echo '[A] ' . yellow('[EXIT]') . '   exited with status code ', (string) $status, PHP_EOL;
 });
 
-$processB = $factory->spawn('php', array('-r', 'foreach (range(1, 6) as $i) { echo $i, PHP_EOL; sleep(1); } '));
+$processB = $factory->spawn('php', array('-r', 'foreach (range(1, 6) as $i) { echo $i, PHP_EOL; sleep(1); } fputs(STDERR, "Bye.");'));
 
 $processB->stdout->on('data', function ($data) {
-    echo "[B] [STDOUT]: ";
+    echo '[B] ' . green('[STDOUT]') . ' ';
     var_dump($data);
 });
 
 $processB->stderr->on('data', function ($data) {
-    echo "[B] [STDERR]: ";
+    echo '[B] ' . red('[STDERR]') . ' ';
     var_dump($data);
 });
 
 $processB->on('exit', function ($status) {
-    echo "[B] [EXIT]:";
-    var_dump($status);
+    echo '[B] ' . yellow('[EXIT]') . '   exited with status code ', (string) $status, PHP_EOL;
 });
 
 $loop->run();
+
+function red($str) {
+    return "\033[31m{$str}\033[0m";
+}
+
+function green($str) {
+    return "\033[32m{$str}\033[0m";
+}
+
+function yellow($str) {
+    return "\033[33m{$str}\033[0m";
+}
