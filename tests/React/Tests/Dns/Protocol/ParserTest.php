6021d636e6c71b73cc53442187d8d41279338a5a
Igor Wiedler <igor@wiedler.ch>
2012-08-19 05:49:55 +0200

    [dns] Make two question test only test parseQuestion

--- tests/React/Tests/Dns/Protocol/ParserTest.php
+++ tests/React/Tests/Dns/Protocol/ParserTest.php
@@ -60,46 +60,6 @@ class ParserTest extends \PHPUnit_Framework_TestCase
         $this->assertSame(Message::CLASS_IN, $request->questions[0]['class']);
     }
 
-    public function testParseRequestWithTwoQuestions()
-    {
-        $data = "";
-        $data .= "72 62 01 00 00 02 00 00 00 00 00 00";     // header
-        $data .= "04 69 67 6f 72 02 69 6f 00";              // question: igor.io
-        $data .= "00 01 00 01";                             // question: type A, class IN
-        $data .= "03 77 77 77 04 69 67 6f 72 02 69 6f 00";  // question: www.igor.io
-        $data .= "00 01 00 01";                             // question: type A, class IN
-
-        $data = $this->convertTcpDumpToBinary($data);
-
-        $request = new Message();
-
-        $parser = new Parser();
-        $parser->parseChunk($data, $request);
-
-        $header = $request->header;
-        $this->assertSame(0x7262, $header->get('id'));
-        $this->assertSame(2, $header->get('qdCount'));
-        $this->assertSame(0, $header->get('anCount'));
-        $this->assertSame(0, $header->get('nsCount'));
-        $this->assertSame(0, $header->get('arCount'));
-        $this->assertSame(0, $header->get('qr'));
-        $this->assertSame(Message::OPCODE_QUERY, $header->get('opcode'));
-        $this->assertSame(0, $header->get('aa'));
-        $this->assertSame(0, $header->get('tc'));
-        $this->assertSame(1, $header->get('rd'));
-        $this->assertSame(0, $header->get('ra'));
-        $this->assertSame(0, $header->get('z'));
-        $this->assertSame(Message::RCODE_OK, $header->get('rcode'));
-
-        $this->assertCount(2, $request->questions);
-        $this->assertSame('igor.io', $request->questions[0]['name']);
-        $this->assertSame(Message::TYPE_A, $request->questions[0]['type']);
-        $this->assertSame(Message::CLASS_IN, $request->questions[0]['class']);
-        $this->assertSame('www.igor.io', $request->questions[1]['name']);
-        $this->assertSame(Message::TYPE_A, $request->questions[1]['type']);
-        $this->assertSame(Message::CLASS_IN, $request->questions[1]['class']);
-    }
-
     public function testParseResponse()
     {
         $data = "";
@@ -145,6 +105,32 @@ class ParserTest extends \PHPUnit_Framework_TestCase
         $this->assertSame(Message::CLASS_IN, $response->answers[0]->class);
         $this->assertSame(86400, $response->answers[0]->ttl);
         $this->assertSame('178.79.169.131', $response->answers[0]->data);
+    }
+
+    public function testParseQuestionWithTwoQuestions()
+    {
+        $data = "";
+        $data .= "04 69 67 6f 72 02 69 6f 00";              // question: igor.io
+        $data .= "00 01 00 01";                             // question: type A, class IN
+        $data .= "03 77 77 77 04 69 67 6f 72 02 69 6f 00";  // question: www.igor.io
+        $data .= "00 01 00 01";                             // question: type A, class IN
+
+        $data = $this->convertTcpDumpToBinary($data);
+
+        $request = new Message();
+        $request->header->set('qdCount', 2);
+        $request->data = $data;
+
+        $parser = new Parser();
+        $parser->parseQuestion($request);
+
+        $this->assertCount(2, $request->questions);
+        $this->assertSame('igor.io', $request->questions[0]['name']);
+        $this->assertSame(Message::TYPE_A, $request->questions[0]['type']);
+        $this->assertSame(Message::CLASS_IN, $request->questions[0]['class']);
+        $this->assertSame('www.igor.io', $request->questions[1]['name']);
+        $this->assertSame(Message::TYPE_A, $request->questions[1]['type']);
+        $this->assertSame(Message::CLASS_IN, $request->questions[1]['class']);
     }
 
     public function testParseAnswerWithInlineData()
