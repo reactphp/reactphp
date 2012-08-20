aaf5aa3847ff06af2ab29737bcd2119cb627e3a6
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-08-21 01:39:03 +0900

    Add a test

--- tests/React/Tests/ChildProcess/ProcessTest.php
+++ tests/React/Tests/ChildProcess/ProcessTest.php
@@ -18,6 +18,18 @@ class ProcessTest extends \PHPUnit_Framework_TestCase
         $this->assertSame('echo foo bar', $process->getCommand());
     }
 
+    public function testIsRunning()
+    {
+        $process = new Process(
+            $this->createProcessStream('echo foo bar'),
+            $this->getMock('\\React\\Stream\\WritableStreamInterface'),
+            $this->getMock('\\React\\Stream\\ReadableStreamInterface'),
+            $this->getMock('\\React\\Stream\\ReadableStreamInterface')
+        );
+
+        $this->assertTrue($process->isRunning());
+    }
+
     private function createProcessStream($command)
     {
         $fdSpecs = array(
