57ccee72a25ba930b0bb8cc152fc4508351e7e0e
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-08-21 02:08:22 +0900

    Add a test

--- tests/React/Tests/ChildProcess/ProcessTest.php
+++ tests/React/Tests/ChildProcess/ProcessTest.php
@@ -30,6 +30,22 @@ class ProcessTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($process->isRunning());
     }
 
+    public function testIsRunningIsFalseWhenTerminated()
+    {
+        $process = new Process(
+            $stream = $this->createProcessStream('sleep 3'),
+            $this->getMock('\\React\\Stream\\WritableStreamInterface'),
+            $this->getMock('\\React\\Stream\\ReadableStreamInterface'),
+            $this->getMock('\\React\\Stream\\ReadableStreamInterface')
+        );
+
+        // proc_terminate() seems not work well
+        // https://bugs.php.net/bug.php?id=39992
+        exec('kill -9 ' . $process->getPid());
+
+        $this->assertFalse($process->isRunning());
+    }
+
     private function createProcessStream($command)
     {
         $fdSpecs = array(
