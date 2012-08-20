490cc67641244ab67b9b3cd94325b35146e85852
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-08-21 02:18:03 +0900

    Refactoring

--- tests/React/Tests/ChildProcess/ProcessTest.php
+++ tests/React/Tests/ChildProcess/ProcessTest.php
@@ -8,42 +8,37 @@ class ProcessTest extends \PHPUnit_Framework_TestCase
 {
     public function testGetCommand()
     {
-        $process = new Process(
-            $this->createProcessStream('echo foo bar'),
-            $this->getMock('\\React\\Stream\\WritableStreamInterface'),
-            $this->getMock('\\React\\Stream\\ReadableStreamInterface'),
-            $this->getMock('\\React\\Stream\\ReadableStreamInterface')
-        );
+        $process = $this->createProcess('echo foo bar');
 
         $this->assertSame('echo foo bar', $process->getCommand());
     }
 
     public function testIsRunning()
     {
-        $process = new Process(
-            $this->createProcessStream('echo foo bar'),
-            $this->getMock('\\React\\Stream\\WritableStreamInterface'),
-            $this->getMock('\\React\\Stream\\ReadableStreamInterface'),
-            $this->getMock('\\React\\Stream\\ReadableStreamInterface')
-        );
+        $process = $this->createProcess('echo foo bar');
 
         $this->assertTrue($process->isRunning());
     }
 
     public function testIsRunningIsFalseWhenTerminated()
     {
-        $process = new Process(
-            $this->createProcessStream('sleep 3'),
-            $this->getMock('\\React\\Stream\\WritableStreamInterface'),
-            $this->getMock('\\React\\Stream\\ReadableStreamInterface'),
-            $this->getMock('\\React\\Stream\\ReadableStreamInterface')
-        );
+        $process = $this->createProcess('sleep 5');
 
         // proc_terminate() seems not work well
         // https://bugs.php.net/bug.php?id=39992
         exec('kill -9 ' . $process->getPid());
 
         $this->assertFalse($process->isRunning());
+    }
+
+    private function createProcess($command)
+    {
+        return new Process(
+            $this->createProcessStream($command),
+            $this->getMock('\\React\\Stream\\WritableStreamInterface'),
+            $this->getMock('\\React\\Stream\\ReadableStreamInterface'),
+            $this->getMock('\\React\\Stream\\ReadableStreamInterface')
+        );
     }
 
     private function createProcessStream($command)
