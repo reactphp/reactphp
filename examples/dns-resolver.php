393af4d4f87bdaeec4fe985022211638341bf558
Igor Wiedler <igor@wiedler.ch>
2012-08-22 21:39:18 +0200

    [dns] Shift things around in dns example

--- examples/dns-resolver.php
+++ examples/dns-resolver.php
@@ -5,11 +5,11 @@
 require __DIR__.'/../vendor/autoload.php';
 
 $loop = React\EventLoop\Factory::create();
+$factory = new React\Dns\Resolver\Factory();
+$resolver = $factory->create('8.8.8.8', $loop);
 
 $domain = 'igor.io';
 
-$factory = new React\Dns\Resolver\Factory();
-$resolver = $factory->create('8.8.8.8:53', $loop);
 $resolver->resolve($domain, function ($ip) {
     echo "Host: $ip\n";
 });
