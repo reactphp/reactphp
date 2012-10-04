8e069b0cde4d2ab2440a92b1c5669781bcfbae3b
Gautam Guliani <gautam@guliani.net>
2012-10-04 16:04:56 +0000

    adding failing test for POSTS with large payloads

--- tests/React/Tests/Http/RequestHeaderParserTest.php
+++ tests/React/Tests/Http/RequestHeaderParserTest.php
@@ -114,6 +114,37 @@ class RequestHeaderParserTest extends TestCase
         $this->assertSame('Maximum header size of 4096 exceeded.', $error->getMessage());
     }
 
+    public function testPostHandlesLargePayloads()
+    {
+        $request = null;
+	$bodyBuffer = null;
+        $parser = new RequestHeaderParser();
+        $parser->on('headers', function ($parsedRequest, $parsedBodyBuffer) use (&$request, &$bodyBuffer) {
+            $request = $parsedRequest;
+            $bodyBuffer = $parsedBodyBuffer;
+        });
+
+        $data = $this->createLargePostRequest();
+        $parser->feed($data);
+
+        $this->assertInstanceOf('React\Http\Request', $request);
+        $this->assertSame('POST', $request->getMethod());
+        $this->assertSame('/foo', $request->getPath());
+        $this->assertSame(array('bar' => 'baz'), $request->getQuery());
+        $this->assertSame('1.1', $request->getHttpVersion());
+        $headers = array(
+            'Host' => 'example.com:80',
+            'User-Agent' => 'Guzzle/2.0 (Language=PHP/5.3.8; curl=7.21.4; Host=universal-apple-darwin11.0)',
+            'Connection' => 'close',
+        );
+        $this->assertSame($headers, $request->getHeaders());
+	$this->assertTrue(strlen($bodyBuffer) > 10);
+    }
+
+
+
+
+
     private function createGetRequest()
     {
         $data = "GET / HTTP/1.1\r\n";
@@ -134,4 +165,24 @@ class RequestHeaderParserTest extends TestCase
 
         return $data;
     }
+
+    private function createLargePostRequest()
+    {
+
+
+        $largePayload = str_repeat('A', 4097);
+	$payloadSize = strlen($largePayload);
+
+        $data = "POST /foo?bar=baz HTTP/1.1\r\n";
+        $data .= "Host: example.com:80\r\n";
+        $data .= "User-Agent: Guzzle/2.0 (Language=PHP/5.3.8; curl=7.21.4; Host=universal-apple-darwin11.0)\r\n";
+        $data .= "Connection-type: text/plain\r\n";
+        $data .= "Connection-length: $payloadSize\r\n";
+        $data .= "Connection: close\r\n";
+        $data .= $largePayload;
+        $data .= "\r\n";
+
+        return $data;
+    }
+
 }
