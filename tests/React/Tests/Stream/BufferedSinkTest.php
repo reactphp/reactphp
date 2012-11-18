27990330e39d6363ec025be53beadcf584809628
Igor Wiedler <igor@wiedler.ch>
2012-11-18 01:18:39 +0100

    Implement BufferedSink which resembles a promise-based async stream_get_contents()

---
+++ tests/React/Tests/Stream/BufferedSinkTest.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace React\Tests\Stream;
+
+use React\Stream\BufferedSink;
+use React\Stream\ReadableStream;
+use React\Tests\Socket\TestCase;
+
+/**
+ * @covers React\Stream\BufferedSink
+ */
+class BufferedSinkTest extends TestCase
+{
+    /** @test */
+    public function promiseShouldReturnPromise()
+    {
+        $sink = new BufferedSink();
+        $contents = $sink->promise();
+
+        $this->assertInstanceOf('React\Promise\PromiseInterface', $contents);
+    }
+
+    /** @test */
+    public function endShouldResolvePromiseWithBufferContents()
+    {
+        $callback = $this->expectCallableOnceWith('foo');
+
+        $sink = new BufferedSink();
+        $sink
+            ->promise()
+            ->then($callback);
+
+        $sink->write('foo');
+        $sink->end();
+    }
+
+    /** @test */
+    public function closeWithEmptyBufferShouldResolveToEmptyString()
+    {
+        $callback = $this->expectCallableOnceWith('');
+
+        $sink = new BufferedSink();
+        $sink
+            ->promise()
+            ->then($callback);
+
+        $sink->close();
+        $sink->close();
+    }
+
+    /** @test */
+    public function closeTwiceShouldBeFine()
+    {
+        $callback = $this->expectCallableOnce();
+
+        $sink = new BufferedSink();
+        $sink
+            ->promise()
+            ->then($callback);
+
+        $sink->close();
+        $sink->close();
+    }
+
+    /** @test */
+    public function resovedValueShouldContainMultipleWrites()
+    {
+        $callback = $this->expectCallableOnceWith('foobarbaz');
+
+        $sink = new BufferedSink();
+        $sink
+            ->promise()
+            ->then($callback);
+
+        $sink->write('foo');
+        $sink->write('bar');
+        $sink->write('baz');
+        $sink->end();
+    }
+
+    /** @test */
+    public function dataWrittenOnEndShouldBeBuffered()
+    {
+        $callback = $this->expectCallableOnceWith('foobar');
+
+        $sink = new BufferedSink();
+        $sink
+            ->promise()
+            ->then($callback);
+
+        $sink->write('foo');
+        $sink->end('bar');
+    }
+
+    /** @test */
+    public function errorsShouldRejectPromise()
+    {
+        $errback = $this->expectCallableOnceWith($this->callback(function ($e) {
+            return $e instanceof \Exception && 'Shit happens' === $e->getMessage();
+        }));
+
+        $sink = new BufferedSink();
+        $sink
+            ->promise()
+            ->then($this->expectCallableNever(), $errback);
+
+        $sink->emit('error', array(new \Exception('Shit happens')));
+    }
+
+    /** @test */
+    public function forwardedErrorsFromPipeShouldRejectPromise()
+    {
+        $errback = $this->expectCallableOnceWith($this->callback(function ($e) {
+            return $e instanceof \Exception && 'Shit happens' === $e->getMessage();
+        }));
+
+        $sink = new BufferedSink();
+        $sink
+            ->promise()
+            ->then($this->expectCallableNever(), $errback);
+
+        $readable = new ReadableStream();
+        $readable->pipe($sink);
+        $readable->emit('error', array(new \Exception('Shit happens')));
+    }
+
+    /** @test */
+    public function pipeShouldSucceedAndResolve()
+    {
+        $callback = $this->expectCallableOnceWith('foobar');
+
+        $sink = new BufferedSink();
+        $sink
+            ->promise()
+            ->then($callback);
+
+        $readable = new ReadableStream();
+        $readable->pipe($sink);
+        $readable->emit('data', array('foo'));
+        $readable->emit('data', array('bar'));
+        $readable->close();
+    }
+
+    private function expectCallableOnceWith($value)
+    {
+        $callback = $this->createCallableMock();
+        $callback
+            ->expects($this->once())
+            ->method('__invoke')
+            ->with($value);
+
+        return $callback;
+    }
+}
