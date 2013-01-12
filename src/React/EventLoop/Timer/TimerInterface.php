1125831c6c9daffd9f3f8e3493370021d1d6a2a8
Daniele Alessandri <suppakilla@gmail.com>
2013-01-12 12:22:13 +0100

    [EventLoop] Add a timer interface and class.
    They will be used to provide a much nicer abstraction for timers shared
    among the various event loop implementations.

---
+++ src/React/EventLoop/Timer/TimerInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace React\EventLoop\Timer;
+
+interface TimerInterface
+{
+    public function getLoop();
+    public function getInterval();
+    public function getCallback();
+    public function setData($data);
+    public function getData();
+    public function isPeriodic();
+    public function cancel();
+}
