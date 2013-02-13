79932b6c950f2e96764d55c7a67ef652c5626ad3
Andrey <andrey.kalinovsky@gmail.com>
2013-02-13 18:10:51 +0100

    Fixed interface mentionning uv constants

--- src/React/Filesystem/FilesystemInterface.php
+++ src/React/Filesystem/FilesystemInterface.php
@@ -4,8 +4,8 @@ namespace React\Filesystem;
 
 interface FilesystemInterface
 {
-  public function mkdir($dirname, $permissions = 0755);
-  public function open($path, $flags = UV::O_WRONLY, $mode = 0644);
+  public function mkdir($dirname, $permissions);
+  public function open($path, $flags, $mode);
   public function write($fd, $buffer, $offset);
   public function read($fd, $length);
   public function close($fd);
