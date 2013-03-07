a8ef2652cb562ece830fef87b6af10abead64430
Igor Wiedler <igor@wiedler.ch>
2013-03-07 17:13:20 +0100

    Basic benchmark scripts

---
+++ benchmark/timers.php
@@ -0,0 +1,25 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/bench.php';
+
+$tests = array(
+    '1000 one-off timers' => function ($loop) {
+        for ($i = 0; $i < 1000; $i++) {
+            $loop->addTimer(1, function ($signature, $loop) {});
+        }
+        $loop->run();
+    },
+    '1000 periodic timers' => function ($loop) {
+        for ($i = 0; $i < 1000; $i++) {
+            $loop->addPeriodicTimer(1, function ($signature, $loop) use (&$i) {
+                if ($i >= 1000) {
+                    $loop->cancelTimer($signature);
+                }
+            });
+        }
+        $loop->run();
+    },
+);
+
+benchLoops($tests);
