caeb286d9aff9e0d9b5ef5e221a622af0719578b
Andrey <andrey.kalinovsky@gmail.com>
2013-04-02 10:09:15 +0200

    Replaced array in FlagConverter with a static

--- src/React/Filesystem/FlagConverter/LibUvFlagConverter.php
+++ src/React/Filesystem/FlagConverter/LibUvFlagConverter.php
@@ -6,23 +6,17 @@ use React\Filesystem\FilesystemInterface;
 
 class LibUvFlagConverter implements FlagConverterInterface
 {
-    private $flags;
-        
-    public function __construct()
-    {
-        $this->flags = array(
+    static private $flags = array(
             FilesystemInterface::FLAG_RDONLY => \UV::O_RDONLY,
             FilesystemInterface::FLAG_WRONLY => \UV::O_WRONLY,
             FilesystemInterface::FLAG_CREAT => \UV::O_CREAT,
             FilesystemInterface::FLAG_APPEND => \UV::O_APPEND
             );
-    }
     
     public function convertFlags($flags)
     {
         $res = 0;
-        
-        foreach ($this->flags as $fsFlag => $uvFlag) {
+        foreach (self::$flags as $fsFlag => $uvFlag) {
             if (($flags & $fsFlag)) {
                 $res |= $uvFlag;
             }
