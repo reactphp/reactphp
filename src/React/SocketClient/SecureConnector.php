53725247e89bb0f933eed2a9a18ecd52ed5cb2b1
Chris Boden <cboden@gmail.com>
2013-04-14 17:14:39 -0400

    CS

--- src/React/SocketClient/SecureConnector.php
+++ src/React/SocketClient/SecureConnector.php
@@ -9,13 +9,11 @@ use React\Promise\When;
 class SecureConnector implements ConnectorInterface
 {
     private $connector;
-    private $loop;
     private $streamEncryption;
 
     public function __construct(ConnectorInterface $connector, LoopInterface $loop)
     {
         $this->connector = $connector;
-        $this->loop = $loop;
         $this->streamEncryption = new StreamEncryption($loop);
     }
 
