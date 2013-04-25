518b127a0cc89a028db8404edb92ef3db24dbaa1
Christian Lück <christian@lueck.tv>
2013-04-25 21:32:41 +0200

    Make sure given resources are of type "socket" and reject otherwise

--- src/React/EventLoop/SocketSelectLoop.php
+++ src/React/EventLoop/SocketSelectLoop.php
@@ -2,8 +2,26 @@
 
 namespace React\EventLoop;
 
+use InvalidArgumentException;
+
 class SocketSelectLoop extends AbstractSelectLoop
 {
+    
+    public function addReadStream($stream, $listener)
+    {
+        if (get_resource_type($stream) !== 'socket') {
+            throw new InvalidArgumentException('Socket loop only accepts resources of type "socket"');
+        }
+        return parent::addReadStream($stream, $listener);
+    }
+    
+    public function addWriteStream($stream, $listener)
+    {
+        if (get_resource_type($stream) !== 'socket') {
+            throw new InvalidArgumentException('Socket loop only accepts resources of type "socket"');
+        }
+        return parent::addWriteStream($stream, $listener);
+    }
 
     protected function select(&$read, &$write, &$except, $utime)
     {
