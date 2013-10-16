4ee261b09286e74f4a4e8926ccd8cd47670cd713
Jeremy Mikola <jmikola@gmail.com>
2013-10-16 13:20:46 -0400

    ChildProcess tests do run with libevent on Linux

--- tests/React/Tests/ChildProcess/LibEventLoopProcessTest.php
+++ tests/React/Tests/ChildProcess/LibEventLoopProcessTest.php
@@ -8,10 +8,6 @@ class LibEventLoopProcessTest extends AbstractProcessTest
 {
     public function createLoop()
     {
-        if ('Linux' === PHP_OS) {
-            $this->markTestSkipped('libevent tests skipped on linux due to linux epoll issues.');
-        }
-
         if (!function_exists('event_base_new')) {
             $this->markTestSkipped('libevent tests skipped because ext-libevent is not installed.');
         }
