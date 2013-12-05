ad77e91256fa2a4d87f54155b0e17138f6e27de0
Daniele Alessandri <suppakilla@gmail.com>
2013-12-05 17:06:16 +0100

    Update scripts in examples directory after event loop API change.
    NOTE: http-client.php still fails since the socket client component
    needs some work to be adapted to the new event loop API.

--- examples/scalability.php
+++ examples/scalability.php
@@ -8,12 +8,13 @@ $loop = React\EventLoop\Factory::create();
 
 for ($i=0;$i<10;++$i) {
     $s=stream_socket_server('tcp://127.0.0.1:'.(8000+$i));
-    $loop->addReadStream($s, function ($s) use ($i) {
+    $loop->onReadable($s, function ($s) use ($i) {
         $c=stream_socket_accept($s);
         $len=strlen($i)+4;
         fwrite($c,"HTTP/1.1 200 OK\r\nContent-Length: $len\r\n\r\nHi:$i\n");
         echo "Served on port 800$i\n";
     });
+    $loop->enableRead($s);
 }
 
 echo "Access your brand new HTTP server on 127.0.0.1:800x. Replace x with any number from 0-9\n";
