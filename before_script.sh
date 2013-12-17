19c763268bd25eabd42be07324444c89be0b59bd
wyrihaximus <ceesjank@gmail.com>
2013-12-17 20:57:06 +0100

    Return to the correct directory after compiling ext

--- before_script.sh
+++ before_script.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y libevent-dev
 if [ "$TRAVIS_PHP_VERSION" != "hhvm" ] && [ "\$(php --re libevent | grep 'does not exist')" != "" ]; then
      wget http://pecl.php.net/get/libevent-0.0.5.tgz;
     tar -xzf libevent-0.0.5.tgz;
-    cd libevent-0.0.5 && phpize && ./configure && make && sudo make install;
+    cd libevent-0.0.5 && phpize && ./configure && make && sudo make install && cd ../;
     echo "extension=libevent.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`;
 fi
 composer self-update
