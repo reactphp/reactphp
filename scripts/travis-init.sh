a9b2a15da5750849d2e316fe102c18aaa689eb3f
Chris Boden <cboden@gmail.com>
2014-01-26 14:19:33 -0500

    Re-enable LibEv tests on Travis

--- scripts/travis-init.sh
+++ scripts/travis-init.sh
@@ -7,6 +7,11 @@ if [ "$TRAVIS_PHP_VERSION" != "hhvm" ] && [ "\$(php --re libevent | grep 'does n
     cd libevent-0.0.5 && phpize && ./configure && make && sudo make install && cd ../;
     echo "extension=libevent.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`;
 fi
+
 echo "yes" | pecl install event
+
+(git clone --recursive https://github.com/m4rw3r/php-libev && cd php-libev && phpize && ./configure --with-libev && make && make install)
+echo "extension=libev.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+
 composer self-update
 composer install --dev --prefer-source
