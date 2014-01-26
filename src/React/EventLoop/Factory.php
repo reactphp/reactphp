b213e33b3e058cc3c7b84551a2803fa3c3bdc8f9
Chris Boden <cboden@gmail.com>
2014-01-26 13:22:00 -0500

    [EventLoop] Factory more implementations

--- src/React/EventLoop/Factory.php
+++ src/React/EventLoop/Factory.php
@@ -2,9 +2,6 @@
 
 namespace React\EventLoop;
 
-use React\EventLoop\StreamSelectLoop;
-use React\EventLoop\LibEventLoop;
-
 class Factory
 {
     public static function create()
@@ -12,6 +9,10 @@ class Factory
         // @codeCoverageIgnoreStart
         if (function_exists('event_base_new')) {
             return new LibEventLoop();
+        } else if (class_exists('libev\EventLoop')) {
+            return new LibEvLoop;
+        } else if (class_exists('EventBase')) {
+            return new ExtEventLoop;
         }
 
         return new StreamSelectLoop();
