498689c0f837c51035ca7ddfb3011d5551ac672a
Christian Lück <christian@lueck.tv>
2014-01-28 00:59:05 +0100

    Consistent naming for packages

--- scripts/do-split.sh
+++ scripts/do-split.sh
@@ -13,9 +13,9 @@ $GIT_SUBSPLIT publish "
     src/React/EventLoop:git@github.com:reactphp/event-loop.git
     src/React/Stream:git@github.com:reactphp/stream.git
     src/React/Cache:git@github.com:reactphp/cache.git
-    src/React/Socket:git@github.com:reactphp/socket.git
-    src/React/SocketClient:git@github.com:reactphp/socket-client.git
-    src/React/Http:git@github.com:reactphp/http.git
-    src/React/HttpClient:git@github.com:reactphp/http-client.git
+    src/React/Socket/Server:git@github.com:reactphp/socket-server.git
+    src/React/Socket/Client:git@github.com:reactphp/socket-client.git
+    src/React/Http/Server:git@github.com:reactphp/http-server.git
+    src/React/Http/Client:git@github.com:reactphp/http-client.git
     src/React/Dns:git@github.com:reactphp/dns.git
 " --heads=master
