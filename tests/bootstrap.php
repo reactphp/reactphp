71d550454b7b4d092be8ff2442e69b45ffe35a80
Christian Lück <christian@lueck.tv>
2019-01-26 14:15:21 +0100

    Backwards compatibility with Promise v1 API
    This simple patch updates this project to target either the current
    Promise v2 API or the legacy Promise v1 API. While we actively encourage
    people to use the current Promise v2 API, we still provide active
    support for the old Promsie v1 API. For the most part, they are fully
    compatible, see also the
    https://github.com/reactphp/promise/blob/2.x/CHANGELOG.md for details.
    
    The legacy Promise v1 API also supports outdated legacy PHP 5.3 which we
    understand is still a requirement for a certain non-zero number of
    projects. That being said, we actively encourage our users to update to
    the latest version of PHP 7+ for best performance.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -18,5 +18,14 @@ foreach (glob(__DIR__ . '/../vendor/react/*/composer.json') as $b) {
 
 // load all legacy test bootstrap scripts from React's components
 foreach (glob(__DIR__ . '/../vendor/react/*/tests/bootstrap.php') as $b) {
+    // skip legacy react/promise for now and use manual autoload path from bootstrap config
+    // @link https://github.com/reactphp/promise/blob/1.x/tests/bootstrap.php
+    // @link https://github.com/reactphp/promise/blob/2.x/tests/bootstrap.php
+    if (strpos($b, 'react/promise/tests/bootstrap.php') !== false) {
+        $autoload->add('React\Promise', __DIR__ . '/../vendor/react/promise/tests');
+        $autoload->addPsr4('React\\Promise\\', __DIR__ . '/../vendor/react/promise/tests');
+        continue;
+    }
+
     include $b;
 }
