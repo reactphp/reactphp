24352c38e5827a15527977d0aaa5925d32d926a6
Christian Lück <christian@clue.engineering>
2023-11-16 18:31:40 +0100

    Update test suite to remove legacy test bootstrapping

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,9 @@
 
 $autoload = require __DIR__ . '/../vendor/autoload.php';
 
-// register all `autoload-dev` paths from React's components
+assert($autoload instanceof Composer\Autoload\ClassLoader);
+
+// register all `autoload-dev` paths from ReactPHP's components
 foreach (glob(__DIR__ . '/../vendor/react/*/composer.json') as $b) {
     $config = json_decode(file_get_contents($b), true);
 
@@ -14,18 +16,4 @@ foreach (glob(__DIR__ . '/../vendor/react/*/composer.json') as $b) {
             }
         }
     }
-}
-
-// load all legacy test bootstrap scripts from React's components
-foreach (glob(__DIR__ . '/../vendor/react/*/tests/bootstrap.php') as $b) {
-    // skip legacy react/promise for now and use manual autoload path from bootstrap config
-    // @link https://github.com/reactphp/promise/blob/1.x/tests/bootstrap.php
-    // @link https://github.com/reactphp/promise/blob/2.x/tests/bootstrap.php
-    if (strpos($b, 'react/promise/tests/bootstrap.php') !== false) {
-        $autoload->add('React\Promise', __DIR__ . '/../vendor/react/promise/tests');
-        $autoload->addPsr4('React\\Promise\\', __DIR__ . '/../vendor/react/promise/tests');
-        continue;
-    }
-
-    include $b;
 }
